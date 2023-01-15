beca4c99fa83d9175fcaee36bb72a382c733e167
Kai0so <kaioaugustoalmeida.m@gmail.com>
2023-01-15 04:26:21 -0300

    feat: update component index imports

--- front-end/app/src/Components/index.js
+++ front-end/app/src/Components/index.js
@@ -3,5 +3,6 @@ import Subtitle from './homeTitles/Subtitle'
 import HomeTemplate from './homeTemplate/HomeTemplate'
 import LoginButton from './homeButtons/LoginButton'
 import RegisterButton from './homeButtons/RegisterButton'
+import OrderOptions from './orderOptions/OrderOptions'
 
-export { Title, Subtitle, HomeTemplate, LoginButton, RegisterButton }
+export { Title, Subtitle, HomeTemplate, LoginButton, RegisterButton, OrderOptions }
