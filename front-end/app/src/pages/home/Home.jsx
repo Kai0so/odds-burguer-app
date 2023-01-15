d775daf9a94ce03759e402e2b39daa4b997e4c9f
Kai0so <kaioaugustoalmeida.m@gmail.com>
2023-01-15 00:17:05 -0300

    feat: Create page Home

---
+++ front-end/app/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Title } from '../../Components';
+
+function Home() {
+  return (
+    <div>
+      <Title />
+      <h2>Home Page</h2>
+    </div>
+  );
+}
+
+export default Home;
