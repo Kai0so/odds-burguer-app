73ad5c9c829c84ddf4aa17c5b4d3f54c12592018
Kai0so <kaioaugustoalmeida.m@gmail.com>
2023-01-17 08:55:09 -0300

    feat: Create Login page, render in App and update index.

--- front-end/app/src/pages/index.js
+++ front-end/app/src/pages/index.js
@@ -1,2 +1,3 @@
 import Home from './home/Home'
-export { Home }
+import Login from './login/Login'
+export { Home, Login }
---
+++ front-end/app/src/pages/login/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { RegisterButton, Subtitle, Form, LoginTemplate } from '../../Components';
+
+function Login() {
+  return (
+    <div>
+      <LoginTemplate />
+      <Subtitle />
+      <Form />
+      <RegisterButton />
+    </div>
+  );
+}
+
+export default Login;
