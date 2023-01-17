12e73879d737223770b2ca11c2407eb7e76791a4
Kai0so <kaioaugustoalmeida.m@gmail.com>
2023-01-17 07:31:54 -0300

    feat: Login button redirect to Login page

--- front-end/app/src/Components/homeButtons/LoginButton.jsx
+++ front-end/app/src/Components/homeButtons/LoginButton.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function LoginButton() {
+  const navigate = useNavigate()
 
   return (
     <div>
-      <button type='button'>Entrar</button>
+      <button type='button' onClick={ () => navigate('/login') }>Entrar</button>
     </div>
   );
 }
