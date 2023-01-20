fee00b407171e4039b7703f86292dc68a2600caa
Kai0so <kaioaugustoalmeida.m@gmail.com>
2023-01-20 16:14:51 -0300

    feat: Create Register context and provider

---
+++ front-end/app/src/context/register/RegisterProvider.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import React, { useEffect, useMemo, useState } from 'react';
+import RegisterContext from './RegisterContext';
+
+function RegisterProvider({ children }) {
+  const [registerData, setRegisterData] = useState({});
+
+  const contextObject = useMemo(() => ({
+    registerData, setRegisterData,
+  }), [registerData, setRegisterData]);
+
+  useEffect(() => {
+    setRegisterData({
+      userName: '', age: '', email: '', password: '', role: '',
+    });
+  }, []);
+
+  return (
+    <RegisterContext.Provider value={contextObject}>
+      {children}
+    </RegisterContext.Provider>
+  );
+}
+
+RegisterProvider.propTypes = {
+  children: PropTypes.node,
+}.isRequired;
+
+export default RegisterProvider;
