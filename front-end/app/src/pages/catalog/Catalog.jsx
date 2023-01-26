e022ece79f2fd58ef7e29453318c8b9e6198a15f
Kai0so <kaioaugustoalmeida.m@gmail.com>
2023-01-26 13:03:40 -0300

    feat: Render CatalogTemplate and FoodCards in Catalog page

--- front-end/app/src/pages/catalog/Catalog.jsx
+++ front-end/app/src/pages/catalog/Catalog.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { CatalogNavbar } from '../../components';
+import { CatalogNavbar, CatalogTemplate, FoodCard } from '../../components';
 
 function Catalog() {
+  const cardList = [
+    'https://static-images.ifood.com.br/image/upload/t_low/pratos/d4e6e76d-0f50-4730-adde-6d27d5caee3a/202202181829_OF22_i.jpg',
+    'https://static-images.ifood.com.br/image/upload/t_low/pratos/d4e6e76d-0f50-4730-adde-6d27d5caee3a/202202181835_6L2I_i.jpg',
+    'https://static-images.ifood.com.br/image/upload/t_low/pratos/d4e6e76d-0f50-4730-adde-6d27d5caee3a/202202181836_2D00_i.jpg',
+    'https://static-images.ifood.com.br/image/upload/t_low/pratos/d4e6e76d-0f50-4730-adde-6d27d5caee3a/202202180948_c865_d.jpg',
+    'https://static-images.ifood.com.br/image/upload/t_low/pratos/d4e6e76d-0f50-4730-adde-6d27d5caee3a/202202181837_6UX2_i.jpg',
+  ];
+
   return (
     <div>
       <CatalogNavbar />
+      <CatalogTemplate />
+      <br />
+      {cardList.map((card, index) => <FoodCard key={index} cardImage={card} />)}
     </div>
   );
 }
