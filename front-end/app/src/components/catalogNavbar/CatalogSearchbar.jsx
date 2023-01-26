6e020fb1022903c58ccbeaa25f8fd13060ff1438
Kai0so <kaioaugustoalmeida.m@gmail.com>
2023-01-26 13:00:27 -0300

    feat: Create CatalogSearchBar component

---
+++ front-end/app/src/components/catalogNavbar/CatalogSearchbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CatalogSearchbar() {
+  return (
+    <div>
+      <input type="text" placeholder="Busque seu lanche" />
+      <button type="button">
+        <img src="https://cdn-icons-png.flaticon.com/128/149/149852.png" alt="Pesquisar" />
+      </button>
+    </div>
+  );
+}
+export default CatalogSearchbar;
