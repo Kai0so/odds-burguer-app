7cba38f02db39eea5aa5be1447847394941ee2ab
Kai0so <kaioaugustoalmeida.m@gmail.com>
2023-01-26 11:19:20 -0300

    feat: Create CatalogIcon component

---
+++ front-end/app/src/components/catalogNavbar/CatalogIcon.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function CatalogIcon() {
+  return (
+    <div>
+      <img src="https://static-images.ifood.com.br/image/upload/t_thumbnail/logosgde/d4e6e76d-0f50-4730-adde-6d27d5caee3a/202202181321_CaHX_.jpeg" alt="ícone OddsBurguer" />
+    </div>
+  );
+}
+export default CatalogIcon;
