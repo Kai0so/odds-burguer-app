fd03cd17e36acfc28f1b79f1b4a9f6ce6c82ed75
Kai0so <kaioaugustoalmeida.m@gmail.com>
2023-01-26 12:59:52 -0300

    feat: Create CatalogTemplate component

---
+++ front-end/app/src/components/catalogTemplate/CatalogTemplate.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CatalogTemplate() {
+  return (
+    <div>
+      <img src="https://static-images.ifood.com.br/image/upload//capa/d4e6e76d-0f50-4730-adde-6d27d5caee3a/202206171905_lmBC_i@2x.jpg" alt="Hamburgers" />
+    </div>
+  );
+}
+
+export default CatalogTemplate;
