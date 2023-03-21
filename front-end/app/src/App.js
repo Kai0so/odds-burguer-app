a85eb4a07a97b61e28d8cfdf5d119303e1b8c24a
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2023-03-21 16:25:03 -0300

    feat: give App access to CartProvider

--- front-end/app/src/App.js
+++ front-end/app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import CartProvider from './context/cart/CartProvider';
 import RegisterProvider from './context/register/RegisterProvider';
 import {
   Catalog, Home, Login, Register,
@@ -8,12 +9,14 @@ import {
 function App() {
   return (
     <RegisterProvider>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/catalog" element={<Catalog />} />
-      </Routes>
+      <CartProvider>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/catalog" element={<Catalog />} />
+        </Routes>
+      </CartProvider>
     </RegisterProvider>
   );
 }
